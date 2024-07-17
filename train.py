4b88749a7244db82540aa3c28b4e94895f3480c4
brunoedcf <brest.dallacosta@outlook.com>
2024-07-17 16:23:53 -0300

    update train.py. remove predict.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import torch
 
+EPOCHS=50
 
 def train_model():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -9,8 +10,8 @@ def train_model():
     if device == "cuda":
         print(torch.cuda.get_device_name(torch.cuda.current_device()))
 
-    model = YOLO("yolov8l.pt").to(device)
-    results = model.train(data="config.yaml", epochs=10)
+    model = YOLO("best.pt").to(device)
+    results = model.train(data="config.yaml", epochs=EPOCHS)
 
 
 if __name__ == "__main__":
